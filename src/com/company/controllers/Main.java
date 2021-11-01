badcc6ff698cbe97f24c2eb640a8464ef2f5cc59
Pranay Jha <pranay.jha@gmail.com>
2021-10-31 22:38:17 -0700

    added DBConnect class

--- src/com/company/controllers/Main.java
+++ src/com/company/controllers/Main.java
@@ -4,8 +4,9 @@ import com.company.views.TempCmdLineView;
 
 public class Main {
 
-    public static TempCmdLineView cmdLineView = new TempCmdLineView();
-    public static void main(String[] args) {
+    //public static TempCmdLineView cmdLineView = new TempCmdLineView();
 
+    public static void main(String[] args) {
+        public DBConnect db = new DBConnect("events.db");
     }
 }
